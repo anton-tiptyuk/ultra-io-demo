32800d5276dcfd9d6a6f4d5363e50a97a8272253
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-04-24 22:08:30 +0300

    removed redundant app parts

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { ConfigModule } from './config/config.module';
 import { CarModule } from './car/car.module';
 
 @Module({
   imports: [ConfigModule, CarModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
