1550eb6371b31928da9d31872007c07cedbb8e5b
Anton Tiptyuk <anton.tiptyuk@gmail.com>
2020-04-24 21:41:46 +0300

    typeorm initialization

--- src/config/config.module.ts
+++ src/config/config.module.ts
@@ -1,8 +1,18 @@
 import { Module, Global } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
 import { ConfigService } from './config.service';
 
 @Global()
 @Module({
-  providers: [ConfigService]
+  imports: [
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) =>
+        configService.getOrmConfig()
+    }),
+  ],
+  providers: [ConfigService],
+  exports: [ConfigService],
 })
 export class ConfigModule { }
